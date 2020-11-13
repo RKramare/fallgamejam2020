a8dc83d43974e42a9fc704f60d42107dd4def9e4
Rasmus <raskr891@student.liu.se>
2020-11-13 19:33:10 +0100

    La till main filen för lua löve

---
+++ main.lua
@@ -0,0 +1,15 @@
+function love.load()
+    
+end
+ 
+function love.update(dt)
+    
+end
+
+function love.keypressed(key)
+    
+end
+ 
+function love.draw()
+    love.graphics.print("2020", 100, 100)
+end
