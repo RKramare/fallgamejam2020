13921485e8c114ebbdb563cb6015313ff25c352b
Rasmus <raskr891@student.liu.se>
2020-11-13 20:25:36 +0100

    testing player file

---
+++ player.lua
@@ -0,0 +1,19 @@
+function new()
+    player = {}
+
+    player.x = love.graphics.getWidth() / 2
+    player.y = love.graphics.getHeight() / 2
+
+    player.speed = 200
+
+    player.img = love.graphics.newImage('purple.png')
+
+    player.ground = player.y
+
+    player.y_velocity = 0
+
+    player.jump_height = -300
+    player.gravity = -500
+
+    return player
+end
