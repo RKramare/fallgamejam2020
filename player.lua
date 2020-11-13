8aa9a35c5d42a68bf366883731a843c6fcb3ebeb
Rasmus <raskr891@student.liu.se>
2020-11-13 21:57:12 +0100

    Added box and player dimensions

--- player.lua
+++ player.lua
@@ -8,11 +8,14 @@ function loadPlayer()
 
     player.img = love.graphics.newImage('res/purple.png')
 
+    player.height = player.img:getHeight()
+    player.width = player.img:getWidth()
+
     player.ground = player.y
 
     player.y_velocity = 0
 
-    player.jump_height = -300
+    player.jump_height = -350
     player.gravity = -500
 
     return player
